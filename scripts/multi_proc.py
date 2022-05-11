1358765c8d50ace6045f20c2c97aa101da696f0b
William Longpré <williamlongpr@hotmail.com>
2022-05-11 00:36:56 -0400

    args nb_threads and nb_process for multi_proc.py

--- scripts/multi_proc.py
+++ scripts/multi_proc.py
@@ -5,22 +5,24 @@ import os
 import subprocess
 import sys
 
-NUM_PROCESSES = 2
+NUM_PROCESSES = 1
 
 def run_process(task_desc):
-    p = subprocess.Popen(["./asset_conv"], stdin=subprocess.PIPE)
+    p = subprocess.Popen(["./asset_conv", "-t", "{0}".format(sys.argv[2])], stdin=subprocess.PIPE)
     p.communicate(input=task_desc.encode())
 
 
 
 if __name__ == "__main__":
-    pool = Pool(NUM_PROCESSES)
+    nb_processes = int(sys.argv[1])
+    print("Starting {0} processes with {1} threads each".format(sys.argv[1], sys.argv[2]))
+    pool = Pool(nb_processes)
 
-    queues = [ [] for i in range(NUM_PROCESSES) ]
+    queues = [ [] for i in range(nb_processes) ]
     i = 0
     for line in sys.stdin:
         queues[i].append(line)
-        i = (i + 1) % NUM_PROCESSES
+        i = (i + 1) % nb_processes
 
     task_descs = []
     for q in queues:
